6a1b611cdee337d7496f0d7fcd6d4896cd02d8e8
= <=>
2023-02-13 23:26:56 +0530

    modified

--- server.js
+++ server.js
@@ -7,12 +7,14 @@ import categoriesRoutes from "./routes/categories.js"
 import usersRoutes from "./routes/users.js"
 import mongoose from "mongoose"
 import cors from "cors"
+import path from "path"
 
 dotenv.config()
 
 const app = express()
 mongoose.connect(process.env.MONGO_URL)
 
+app.use(express.static("public"))
 app.use(cors())
 app.use(express.json({ limit: "1mb" }))
 app.use(express.urlencoded({ extended: true }))
@@ -22,6 +24,10 @@ app.use("/api/auth", authRoutes)
 app.use("/api/posts", postsRoutes)
 app.use("/api/users", isAuthenticated, usersRoutes)
 app.use("/api/categories", categoriesRoutes)
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve("public/index.html"))
+})
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening to port ${process.env.PORT}`)
